9a73a0c40b4e5aae6aa9c6f078f1bf0d3f99e753
Vincnet <vincenteliya35@gmail.com>
2022-04-14 22:16:07 +0300

    Modified Allowed host to allow all

--- etour/settings.py
+++ etour/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # DEBUG = str(os.getenv('DEBUG_VALUE') == 'True')
 DEBUG = False
 
-ALLOWED_HOSTS = ['etour.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -181,6 +181,6 @@ CSRF_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = 31536000 # FOR ONE HOUR THE SITE WILL NOT BE ACCESIBLE TO ANY CONNECTION WITH NO SSL
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True # SERVICE PROVIDERS ALSO MUST HAVE AN SSL
 SECURE_HSTS_PRELOAD = True
-ADMINS = [('Vincent', 'etourinternational01@gmail.com'),]
+ADMINS = [('Etour', 'etourinternational01@gmail.com'), ('Vincent', 'vincenteliya35@gmail.com')]
 
 django_heroku.settings(locals())
