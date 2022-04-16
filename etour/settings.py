cbd0899627b853ae4377a0faab916792086c5c8e
Vincnet <vincenteliya35@gmail.com>
2022-04-16 12:28:33 +0300

    Set debug false

--- etour/settings.py
+++ etour/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.getenv('DEBUG_VALUE') == 'True')
-# DEBUG = False
+# DEBUG = str(os.getenv('DEBUG_VALUE') == 'True')
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
