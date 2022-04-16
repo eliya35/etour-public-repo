304458d0f712120df53031917c33c38d1827e057
Vincnet <vincenteliya35@gmail.com>
2022-04-16 14:46:46 +0300

    Modified the alert message in suggestions and contact us form

--- src/components/quicklinks/Suggestions.jsx
+++ src/components/quicklinks/Suggestions.jsx
@@ -133,7 +133,7 @@ const Suggestion = () => {
         axios.post('https://etour.herokuapp.com/HDp0mdCOWxaBRhELG5PUMWQnrXSkObDQBnvUhC5XsTROlI6Wz99ctDZtzRLqHuvgidz0mX3ws3K6ggPc8p21OT2jwEcbpNMDHcHrxb0EoN7al1aP8fKoSpZMyXvL9FxnkJuS2KG5r1d8YkjyYjgCj2V44GdYk6ehB7JJuqoE6wAZWe5VisNMKnFYfS40mhymtJNFb8Aq/suggestions/', data)
             .then(alert("Your suggestion has been sent successfully"))
             .catch(err => {
-                alert('Oopss! something went wrong while sending your message. Please try again after a few minutes.')
+                alert('Oopss! something went wrong while sending your message. Make sure the form is field correctly or try again after a few minutes.')
             })
     }
 
