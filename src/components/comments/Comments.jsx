3bef6dfdba2e28d679bfc2e26bac5ba02a88c646
Vincnet <vincenteliya35@gmail.com>
2022-04-20 19:04:10 +0300

    Working on reverting the changes

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -38,8 +38,7 @@ const Comments = ({ tour_site_id }) => {
             body: body,
             parent_id: parent_id,
             user_id: user.id,
-            comment_avatar: profile.profile_avatar,
-            // comment_avatar_url: profile.profile_avatar,
+            comment_avatar_url: profile.profile_avatar,
             username: user.username,
             created_at: new Date().toISOString(),
             tour_site_id: tour_site_id
@@ -63,7 +62,6 @@ const Comments = ({ tour_site_id }) => {
             )
             .catch(err => {
                 alert("An error occured while posting your comment. Please try again later")
-                console.log('The comment err', err)
             })
     }
 
