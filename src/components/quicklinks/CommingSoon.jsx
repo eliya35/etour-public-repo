17c6b735f89f997c4c8b432b502ccee98db0c526
Vincnet <vincenteliya35@gmail.com>
2022-04-26 23:39:18 +0300

    Fixed view page css and coming soon page css under media queries

--- src/components/quicklinks/CommingSoon.jsx
+++ src/components/quicklinks/CommingSoon.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 import "../Styles/commingsoon.css"
 
 
-const CommingSoon = () => { 
+const CommingSoon = () => {
     return (
         <div className="comming-soon">
             <h1>
-                Comming Soon 
+                Comming Soon!!
             </h1>
             <p>
-                We apologise for any inconvenience but we gat you, we are working on it.
+                We apologise for any inconvenience we are working on it.
             </p>
         </div>
     );
